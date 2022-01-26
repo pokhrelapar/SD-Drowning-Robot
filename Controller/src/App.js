885332e3eac8e6c7da85771b56218ffb80d53db9
Hunter Redhead <hunter.redhead@mavs.uta.edu>
2022-01-26 11:28:47 -0600

    Pushing the Controller Code

---
+++ Controller/src/App.js
@@ -0,0 +1,45 @@
+import './App.css';
+import Controller from './Controller';
+
+function movementCall(key) {
+  // send api call to turn on thrusters in x direction
+  if(key === 'w' || key === 'a' || key === 's' || key === 'd') {
+    console.log(key + ' is pressed');
+  }
+}
+
+function cancelMovementCall(key) {
+  // send api call to turn off thrusters in x direction
+  if(key === 'w' || key === 'a' || key === 's' || key === 'd') {
+    console.log(key + ' is released');
+  }
+}
+
+function App() {
+  const keymap = new Map();
+
+  // detect for start movement
+  window.addEventListener('keydown', (event) => {
+    if(!keymap.has(event.key)) {
+      movementCall(event.key);
+      keymap.set(event.key, true);
+    }
+  });
+
+  // detect for stop movement
+  window.addEventListener('keyup', (event) => {
+    if(keymap.has(event.key)) {
+      cancelMovementCall(event.key);
+      keymap.delete(event.key);
+    }
+  });
+
+
+  return (
+    <div className="App">
+      <Controller/>
+    </div>
+  );
+}
+
+export default App;
